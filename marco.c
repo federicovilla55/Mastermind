713988767f0d8b8e6ea7fe342cda8aa962902ab9
Marco Zanzottera <marcozanzottera@e.email>
2021-12-17 12:03:58 +0100

    Fixed function checkSequence()

--- marco.c
+++ marco.c
@@ -15,25 +15,30 @@ char* strmalloc(int len)
 
 void checkSequence(char secr[], char curr[], int len, int *white, int *black)
 {
-	int i, j, flag, found[len];
+	int i, j, flag, foundInSecr[len], foundInCurr[len];
 
 	for(i=0;i<len;i++) {
-		found[i] = 0;
+		foundInCurr[i] = 0;
+		foundInSecr[i] = 0;
 	}
-
-	flag = 0;
 
 	for(i=0;i<len;i++) {
 		if(curr[i]==secr[i]) {
 			(*black)++;
-			flag = 1;
-			found[i] = 1;
+			foundInCurr[i] = 1;
+			foundInSecr[i] = 1;
 		}
-		for(j=0;j<len && !flag;j++) {
-			if(curr[i]==secr[j] && !found[j]) {
-				(*white)++;
-				flag = 1;
-				found[j] = 1;
+	}
+
+	for(i=0;i<len;i++) {
+		if(!foundInCurr[i]) {
+			flag = 0;
+			for(j=0;j<len && !flag;j++) {
+				if(curr[i]==secr[j] && !foundInSecr[j]) {
+					(*white)++;
+					flag = 1;
+					foundInSecr[j] = 1;
+				}
 			}
 		}
 	}
